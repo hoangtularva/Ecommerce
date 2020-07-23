f1faecbf381cd6b8ec8751fd5c805bf4d9829df2
Hoàng Tú <50492205+hoangtularva@users.noreply.github.com>
2020-07-23 23:53:12 +0700

    route home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -325,56 +325,8 @@ function App() {
           </div>
           
           <Route path="/" exact={true} component={HomeScreen}/>
-          <Route path="/products/:id" component={ProductScreen}/>
+          {/* <Route path="/products/:id" component={ProductScreen}/> */}
 
-          <div className="row">
-            <div className="col-12">
-              <div className="product-info">
-                <div className="tab-content" id="myTabContent">
-                  {/* Start Single Tab */}
-                  <div className="tab-pane fade show active" id="man" role="tabpanel">
-                    <div className="tab-single">
-                      <div className="row">
-                        {/* Render Product */}
-                        {
-                        data.products.map (product => <div className="col-xl-3 col-lg-4 col-md-4 col-12">
-                          <div className="single-product">
-                            <div className="product-img">
-                              <a href="product-details.html">
-                                <img className="default-img" src= {product.image} alt="#" />
-                                <img className="hover-img" src= {product.image} alt="#" />
-                              </a>
-                              <div className="button-head">
-                                <div className="product-action">
-                                  <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i className=" ti-eye" /><span>Quick Shop</span></a>
-                                  <a title="Wishlist" href="#"><i className=" ti-heart " /><span>Add to Wishlist</span></a>
-                                  <a title="Compare" href="#"><i className="ti-bar-chart-alt" /><span>Add to Compare</span></a>
-                                </div>
-                                <div className="product-action-2">
-                                  <a title="Add to cart" href="#">Add to cart</a>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="product-content">
-                              <h3><a href="product-details.html">{product.name}</a></h3>
-                              <div className="product-price">
-                                <span>{product.price}</span>
-                              </div>
-                              <div class="product-brand">{product.brand}</div>
-                              <div class="product-rating">{product.rating} Stars ({product.numReviews})</div>
-                            </div>
-                          </div>
-                        </div>
-                        )}
-
-                      </div>
-                    </div>
-                  </div>
-                  {/*/ End Single Tab */}
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       {/* End Product Area */}
