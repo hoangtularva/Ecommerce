8f77c7289da7fa5c1375860630e90d68738b1eff
hoangtularva <hoangtukg295@gmail.com>
2020-07-24 17:27:26 +0700

    install babel

---
+++ backend/data.js
@@ -0,0 +1,76 @@
+export default {
+    products: [
+        {
+            _id: "1",
+            name: "Balo Bapago Kaya Navy",
+            image: "/images/1.jpg",
+            price: 40,
+            category: "Adidas",
+            rating: 4.5,
+            numReviews: 9 
+        },
+        {
+            _id: "2",
+            name: "Balo B2B04 Black",
+            image: "/images/2.jpg",
+            price: 55,
+            category: "Adidas",
+            rating: 5,
+            numReviews: 5 
+        },
+        {
+            _id: "3",
+            name: "Balo laptop Mikkor The Kalino",
+            image: "/images/3.jpg",
+            price: 30,
+            category: "Crumpler",
+            rating: 4,
+            numReviews: 16
+        },
+        {
+            _id: "4",
+            name: "Balo Bestlife Elegant",
+            image: "/images/4.jpg",
+            price: 40,
+            category: "Crumpler",
+            rating: 5,
+            numReviews: 10 
+        },
+        {
+            _id: "5",
+            name: "Balo laptop Tresette",
+            image: "/images/5.jpg",
+            price: 49,
+            category: "Nike",
+            rating: 3.5,
+            numReviews: 20 
+        },
+        {
+            _id: "6",
+            name: "Balo Tresette 5C205SL Navy",
+            image: "/images/6.jpg",
+            price: 50,
+            category: "Nike",
+            rating: 4.7,
+            numReviews: 13
+        },
+        {
+            _id: "7",
+            name: "Balo Sakos Cyber",
+            image: "/images/7.jpg",
+            price: 60,
+            category: "Jansport",
+            rating: 3.5,
+            numReviews: 24
+        },
+        {
+            _id: "8",
+            name: "Balo Mikkor The Irvin",
+            image: "/images/8.jpg",
+            price: 57,
+            category: "Jansport",
+            rating: 4.8,
+            numReviews: 20
+        },
+    ]
+}
